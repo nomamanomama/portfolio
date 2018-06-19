cdf8db207af87758027be2ba37498722343190c3
Kathy Kennedy <nomamanomama@yahoo.com>
2018-06-19 17:33:15 -0500

    first commit

---
+++ src/pages/About/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>
+    I have always loved problem-solving and design, and I possess an incessant desire to learn.
+    </p>
+    <p>
+    I graduated from UT Austin with a degree in Architecture. After several years, I returned
+     to UT Austin to study Computer Science. While in school, I landed a great job as a game developer
+      and pursued software development as a new career. When I became a mom, I chose to take a break
+       from my development career to focus on family.
+    </p>
+    <p>
+    I rejoined the workforce in the field of technical sales where I use my knowledge of development
+     to assist prospective customers in finding the best technical solution for their needs. I am currently
+      a member of the software team for an engineering firm in the oil and gas industry. As part of my 
+      interest in forever-learning, I completed UT Austin's full stack web development bootcamp where I gained 
+      experience working with current web tools and technologies for front end, back end and database development
+      including HTML 5.0/CSS, Javascript, JQuery, Bootstrap, GitHub, Handlebars, Node.js, React, PHP, Firebase, Mongo, 
+      Mongoose, CLI, (add more)
+      </p>
+    <p>
+      With the fast-paced ever-changing technology landscape, full stack web development has rekindled my love 
+      of programming and has become be both an exciting challenge and a excellent way to continually evolve.
+    </p>
+  </div>
+);
+
+export default About;
